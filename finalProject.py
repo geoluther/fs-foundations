ebf3ec84d2c8fb8a15b8cea73d685b5d01ba5d31
George <my latest commit>
2016-01-15 23:10:53 -0700

    update menu templates

--- finalProject.py
+++ finalProject.py
@@ -21,13 +21,18 @@ def showRestaurants():
 
 
 ## create a new restaurant
-@app.route('/restaurant/new')
+@app.route('/restaurant/new', methods=['GET', 'POST'])
 def newRestaurant():
-    #restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
-    # items = session.query(MenuItem).filter_by(
-    #     restaurant_id=restaurant_id).all()
-    # return jsonify(MenuItems=[i.serialize for i in items])
-    return "page to CREATE a new restaurant done"
+    if request.method == 'POST':
+        restName = Restaurant(
+            name=request.form['name'])
+        session.add(restName)
+        session.commit()
+        #flash("New Restaurant Created")
+        return redirect(url_for('showRestaurants'))
+    else:
+    	return render_template('newRestaurant.html')
+    #return "page to CREATE a new restaurant done"
 
 
 ## show restaurant menu by rest. id
@@ -63,33 +68,38 @@ def deleteRestaurant(restaurant_id):
 
 @app.route('/restaurant/<int:restaurant_id>/new/', methods=['GET', 'POST'])
 def newMenuItem(restaurant_id):
-    if request.method == 'POST':
-        newItem = MenuItem(
-            name=request.form['name'], restaurant_id=restaurant_id)
-        session.add(newItem)
-        session.commit()
-        flash("new menu item created")
-        return redirect(url_for('restaurantMenu', restaurant_id=restaurant_id))
-    else:
-        return render_template('newmenuitem.html', restaurant_id=restaurant_id)
+	if request.method == 'POST':
+		newItem = MenuItem(name=request.form['name'], description=request.form[
+			'description'], price=request.form['price'], course=request.form['course'], restaurant_id=restaurant_id)
+		session.add(newItem)
+		session.commit()
+		return redirect(url_for('restaurantMenu', restaurant_id=restaurant_id))
+	else:
+		return render_template('newmenuitem.html', restaurant_id=restaurant_id)
 
 
 # Task 2: Create route for editMenuItem function here
 
-@app.route('/restaurant/<int:restaurant_id>/<int:menu_id>/edit', methods = ['GET', 'POST'])
+@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/edit',
+           methods=['GET', 'POST'])
 def editMenuItem(restaurant_id, menu_id):
-	editedItem = session.query(MenuItem).filter_by(id = menu_id).one()
-	if request.method == 'POST':
-		if request.form['name']:
-			editedItem.name = request.form['name']
-		session.add(editedItem)
-		session.commit()
-		flash("Menu item edited")
-		return redirect(url_for('restaurantMenu', restaurant_id = restaurant_id))
-	else:
-	#USE THE RENDER_TEMPLATE FUNCTION BELOW TO SEE THE VARIABLES YOU SHOULD USE IN YOUR EDITMENUITEM TEMPLATE
-		return render_template('editmenuitem.html', restaurant_id = restaurant_id,
-			MenuID = menu_id, item = editedItem)
+    editedItem = session.query(MenuItem).filter_by(id=menu_id).one()
+    if request.method == 'POST':
+        if request.form['name']:
+            editedItem.name = request.form['name']
+        if request.form['description']:
+            editedItem.description = request.form['description']
+        if request.form['price']:
+            editedItem.price = request.form['price']
+        if request.form['course']:
+            editedItem.course = request.form['course']
+        session.add(editedItem)
+        session.commit()
+        return redirect(url_for('restaurantMenu', restaurant_id=restaurant_id))
+    else:
+
+        return render_template('editmenuitem.html', restaurant_id = restaurant_id,
+ 			MenuID = menu_id, item = editedItem)
 
     # return "page to edit a menu item. Task 2 complete!"
 
